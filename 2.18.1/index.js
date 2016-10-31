a364a1058f1effe1672d5718fcd168d2474c5d64
Mrdaigithub <346139312@qq.com>
2016-10-31 10:53:45 +0800

    completd 2.18.1

--- 2.18.1/index.js
+++ 2.18.1/index.js
@@ -1,31 +1,45 @@
 "use strict";
 
 const DOC = document;
-let text = DOC.querySelector('#text'),
-    unshift = DOC.querySelector('#unshift'),
-    push = DOC.querySelector('#push'),
-    shift = DOC.querySelector('#shift'),
-    pop = DOC.querySelector('#pop'),
-    dataList = DOC.querySelector('#dataList');
+let $ = (dom)=> DOC.querySelector(dom);
 
-unshift.addEventListener('click',()=>{
-},false);
-
-push.addEventListener('click',()=>{
-    let val = text.value.trim();
-    if (!!val && /^\d*$/.test(val)){
-        let numDom = DOC.createElement('li');
-        numDom.innerHTML = val;
-        DOC.body.appendChild(numDom);
+let dataList = $('#dataList');
+$('#btns').addEventListener('click',(e)=>{
+    let val = $('#text').value.trim(),
+        res = undefined;
+    switch (e.target.id){
+        case 'unshift':
+            if (!!val && /^\d*$/.test(val)){
+                let numDom = DOC.createElement('li');
+                numDom.innerHTML = val;
+                dataList.insertBefore(numDom, dataList.children[0]);
+            }
+            break;
+        case 'push':
+            if (!!val && /^\d*$/.test(val)){
+                let numDom = DOC.createElement('li');
+                numDom.innerHTML = val;
+                dataList.appendChild(numDom);
+            }
+            break;
+        case 'shift':
+            if (dataList.children.length > 0){
+                res = dataList.children[0].innerHTML;
+                dataList.removeChild(dataList.children[0]);
+                alert(res);
+                return res;
+            }
+            return res;
+            break;
+        case 'pop':
+            if (dataList.children.length > 0){
+                res = dataList.children[dataList.children.length-1].innerHTML;
+                dataList.removeChild(dataList.children[dataList.children.length-1]);
+                alert(res);
+                return res;
+            }
+            return res;
+            break;
     }
 },false);
 
-shift.addEventListener('click',()=>{
-
-},false);
-
-pop.addEventListener('click',()=>{
-    if (dataList.children.length > 0){
-        dataList.removeChild(dataList.children[dataList.children.length-1]);
-    }
-},false);
