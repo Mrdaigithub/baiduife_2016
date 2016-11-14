ba0a57d8b8547a0289300492f1e60171c2bcc8a6
Mrdaigithub <346139312@qq.com>
2016-11-14 15:42:07 +0800

    completed 2.19.2

---
+++ 2.19.2/index.js
@@ -0,0 +1,92 @@
+"use strict";
+
+let $ = node=>document.querySelector(node),
+    $$ = node=>document.querySelectorAll(node),
+    root = $('#root'),
+    text = $('#text'),
+    stack = [],
+    intervalTime = 500;
+
+
+let clearColor = ()=>{
+    for (let i=0,div=$$('div'),len=div.length; i<len; i++){
+        if (div[i].style.backgroundColor){
+            div[i].style.backgroundColor = '';
+        }
+    }
+};
+
+let render  = (stack, keyword=null, callback=null) =>{
+    setTimeout(()=>{
+        clearColor();
+        if (!stack.length) {
+            stack = [];
+            if (callback) callback();
+            return;
+        }
+        let currDom = stack.shift();
+        currDom.style.backgroundColor = '#1ad361';
+        if (currDom.firstChild.nodeValue.trim() === keyword) {
+            return true;
+        }
+        render(stack, keyword, callback);
+    },intervalTime)
+};
+
+/**
+ * 多叉树广度优先遍历
+ * @param node
+ */
+let breadthFirstTraversal = (node)=>{
+    let arr = [];
+    arr.push(node);
+    while (arr.length){
+        node = arr.shift();
+        stack.push(node);
+        if (node){
+            for (let i=0,len=node.children.length; i<len; i++){
+                arr.push(node.children[i]);
+            }
+        }
+    }
+};
+
+/**
+ * 多叉树深度优先遍历
+ * @param node
+ */
+let depthFirstTraversal = (node)=>{
+    if (!node || node.children === 0) return;
+    stack.push(node);
+    for (let i=0,len=node.children.length; i<len; i++){
+        depthFirstTraversal(node.children[i]);
+    }
+};
+
+
+
+$('.btn-group').addEventListener('click',(e)=>{
+    switch (e.target.id){
+        case 'bft':
+            if (stack) stack = [];
+            breadthFirstTraversal(root);
+            render(stack);
+            break;
+        case 'dft':
+            depthFirstTraversal(root);
+            render(stack);
+            break;
+        case 'bff':
+            breadthFirstTraversal(root);
+            render(stack, text.value.trim(), ()=>{
+                alert('not found');
+            });
+            break;
+        case 'dff':
+            depthFirstTraversal(root);
+            render(stack, text.value.trim(),  ()=>{
+                alert('not found');
+            });
+            break;
+    }
+},false);
